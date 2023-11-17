cb246310508c94ceb4daa81147594403ae0a31b4
khosro <khosroshariatzadeh@gmail.com>
2023-11-16 21:17:15 -0500

    day 2

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,6 @@
 
 import useAuth from '@/hooks/useAuth'
 import {SearchIcon, BellIcon} from '@heroicons/react/solid'
-import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
 function Header() {
@@ -47,14 +46,12 @@ function Header() {
          <SearchIcon className='hidden h-6 w-6 sm:inline' />
          <p className='hidden lg:inline'>Kids</p>
          <BellIcon className= 'h-6 w-6' />
-         {/* <Link href='/account'> */}
             <img
             onClick={logout} 
             src="https://rb.gy/g1pwyx"
             alt=""
             className="cursor-pointer rounded"
              />
-         {/* </Link> */}
         </div>
     </header>
   )
